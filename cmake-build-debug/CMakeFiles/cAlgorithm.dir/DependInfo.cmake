ef6fd3610ae52ad5558c70ff4722af37e6ad96d6
salmon1802 <757635525@qq.com>
2021-09-14 22:52:11 +0800

    开了新坑

---
+++ cmake-build-debug/CMakeFiles/cAlgorithm.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/code/cAlgorithm/leetcode_datastruct/containsDuplicate.c" "D:/code/cAlgorithm/cmake-build-debug/CMakeFiles/cAlgorithm.dir/leetcode_datastruct/containsDuplicate.c.obj"
+  "D:/code/cAlgorithm/leetcode_datastruct/maxSubArray.c" "D:/code/cAlgorithm/cmake-build-debug/CMakeFiles/cAlgorithm.dir/leetcode_datastruct/maxSubArray.c.obj"
+  "D:/code/cAlgorithm/main.c" "D:/code/cAlgorithm/cmake-build-debug/CMakeFiles/cAlgorithm.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cAlgorithm.dir/leetcode_datastruct/containsDuplicate.c.obj"
+  "CMakeFiles/cAlgorithm.dir/leetcode_datastruct/maxSubArray.c.obj"
+  "CMakeFiles/cAlgorithm.dir/main.c.obj"
+  "cAlgorithm.exe"
+  "cAlgorithm.exe.manifest"
+  "cAlgorithm.pdb"
+  "libcAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
